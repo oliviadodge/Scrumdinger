a1da235f9d987848984955044e29edb2dcc3c880
Olivia <oliviadodge@gmail.com>
2021-09-02 22:50:14 -0700

    Add scrums list view and detail view

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,35 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Olivia  Dodge on 9/2/21.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List {
+            ForEach(scrums) { scrum in
+                NavigationLink(destination: DetailView(scrum: scrum)) {
+                    CardView(scrum: scrum)
+                }
+                .listRowBackground(scrum.color)
+            }
+        }
+        .navigationTitle("Daily Scrums")
+        .navigationBarItems(trailing: Button(action: {}) {
+            Image(systemName: "plus")
+        })
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            ScrumsView(scrums: DailyScrum.data)
+        }
+    }
+}
